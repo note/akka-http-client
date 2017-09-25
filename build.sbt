2d02234baa5d10211bc448bed652990efb8d954d
Michal Sitko <pierwszy1@gmail.com>
2017-09-25 12:09:29 +0200

    wip

--- build.sbt
+++ build.sbt
@@ -9,7 +9,7 @@ scalaVersion := "2.11.8"
 
 
 libraryDependencies ++= Seq(
-  "com.typesafe.akka" %% "akka-http" % "10.0.5",
+  "com.typesafe.akka" %% "akka-http" % "10.0.6+22-2aae0b18+20170511-1652",
   "com.typesafe.play" %% "play-ahc-ws-standalone" % "1.0.0-M3"
 )
 
--- src/main/scala/net/michalsitko/scala/ConnectionLevel.scala
+++ src/main/scala/net/michalsitko/scala/ConnectionLevel.scala
@@ -10,26 +10,33 @@ import akka.stream.ActorMaterializer
 import akka.stream.scaladsl.{Sink, Source}
 import net.michalsitko.scala.utils.{Config, ResultLogger}
 
+import scala.util.{Failure, Success}
+
 object ConnectionLevel extends AnyRef with ResultLogger {
   def main(args: Array[String]): Unit = {
     implicit val system = ActorSystem()
     implicit val materializer = ActorMaterializer()
     implicit val ec = system.dispatcher
 
-    val proxySettings = new InetSocketAddress(Config.proxyHost, Config.proxyPort)
+    val proxyHost = "localhost"
+    val proxyPort = 8888
+
+    val proxySettings = new InetSocketAddress(proxyHost, proxyPort)
     val transport = ClientTransport.proxy(None, proxySettings, ClientConnectionSettings(system))
 
     val connectionFlow =
-      Http().outgoingConnectionUsingTransport("www.scala-lang.org", 443, transport, Http().defaultClientHttpsContext)
+      Http().outgoingConnectionUsingTransport("akka.io", 443, transport, Http().defaultClientHttpsContext)
 
     val responseF = Source.single(HttpRequest(uri = "/"))
       .via(connectionFlow)
       .runWith(Sink.head)
 
-    logResult(responseF).andThen {
+    responseF.andThen {
+      case Success(_) => println("request succeeded")
+      case Failure(_) => println("request failed")
+    }.andThen {
       case _ =>
         Http().shutdownAllConnectionPools()
-        materializer.shutdown()
         system.terminate()
     }
   }
